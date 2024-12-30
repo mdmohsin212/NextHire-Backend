4114e41a9781a374d1e958660b2f55e58f0f15a4
md mohsin <siam.mohsin2005@gmail.com>
2024-12-30 20:06:22 +0600

    fixing-issu

--- job/serializars.py
+++ job/serializars.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from resume.serializers import *
 from .models import *
 
 class CategorieSerializer(serializers.ModelSerializer):
@@ -12,8 +13,15 @@ class CompanySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class JobListingSerializer(serializers.ModelSerializer):
-    # category = serializers.StringRelatedField(many=True)
-    # applicants = serializers.StringRelatedField(many=True)
+    category = serializers.StringRelatedField(many=True)
+    applicants = serializers.SerializerMethodField()
+    employer = serializers.StringRelatedField(many=False)
+    company = serializers.StringRelatedField(many=False)
+    
     class Meta:
-        model = JobListing  
+        model = JobListing
         fields = '__all__'
+
+    def get_applicants(self, obj):
+        applications = obj.applications.all()
+        return JobApplicationSerializer(applications, many=True).data
