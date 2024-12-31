6612e9d0494cdc5ffc753b4b1636702b90be99b0
md mohsin <siam.mohsin2005@gmail.com>
2024-12-31 17:41:29 +0600

    update

--- NextHire/settings.py
+++ NextHire/settings.py
@@ -51,9 +51,6 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
@@ -75,15 +72,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'NextHire.wsgi.app'
+WSGI_APPLICATION = 'NextHire.wsgi.application'
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
