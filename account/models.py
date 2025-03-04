5f06d68eacf5bd13c99b5daf7f6e26e7195e2f37
md mohsin <siam.mohsin2005@gmail.com>
2025-03-04 10:29:26 +0600

    update

--- account/models.py
+++ account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,3 +16,16 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     message = models.TextField()
+    
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) 
+        email_subject = f"Someone Contact with You"
+        email_body = render_to_string('contact.html', {'name' : self.name, 'email' : self.email, 'message' : self.message})
+            
+        email = EmailMultiAlternatives(email_subject, '', to=['siam.mohsin2005@gmail.com'])
+        email.attach_alternative(email_body, "text/html")
+        email.send()
